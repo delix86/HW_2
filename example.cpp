3e0e68e01286e2f74dfa9c036822be83682de1e9
DESKTOP-VTSIMTE\vnaza <vnazarov777@gmail.com>
2022-10-07 20:40:05 +0300

    repair: todo_move_me_out': local function definitions are illegal hello_world F:\Programming\Projects\OTUS\CPP\HomeWork\HW2\example.cpp 8

--- example.cpp
+++ example.cpp
@@ -4,8 +4,8 @@
 
 void example_function() {
 	std::cout << "Hello from example function!" << std::endl;
+}
 
-	void todo_move_me_out() {
-		std::cout << "This function should be located in the separated source file" << std::endl;
-	}
+void todo_move_me_out() {
+	std::cout << "This function should be located in the separated source file" << std::endl;
 }
