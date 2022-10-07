e5fb152c27e9c51da46e739c17d45f0372d93753
DESKTOP-VTSIMTE\vnaza <vnazarov777@gmail.com>
2022-10-07 20:32:30 +0300

    factor out finction todo_move_me_out()

--- example.cpp
+++ example.cpp
@@ -4,4 +4,8 @@
 
 void example_function() {
 	std::cout << "Hello from example function!" << std::endl;
+
+	void todo_move_me_out() {
+		std::cout << "This function should be located in the separated source file" << std::endl;
+	}
 }
